b04f88024efe48a8a12aad439871edee2837c4c8
Sebin Sonu Jose <sebinsonu7@gmail.com>
2024-01-30 14:40:19 +0530

    separate module for sql connection

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -11,11 +11,9 @@ def get_all_products():
     response=[]
 
     for (product_id, name, uom_id, price_per_unit,uom_name) in cursor:
-        response.append
-        (
-            {
+        response.append({
               'product_id':product_id,
-              'name, ':name,
+              'name':name,
               'uom_id':uom_id,
               'price_per_unit':price_per_unit,
               'uom_name':  uom_name,
@@ -23,8 +21,6 @@ def get_all_products():
             }
         )
         
-    
-    
     cnx.close()
     return response
 
