cda791678c4ad525a4e2352e5802af6c2d697a33
Sebin Sonu Jose <sebinsonu7@gmail.com>
2024-01-30 19:27:31 +0530

    products backend fixed

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -23,7 +23,28 @@ def get_all_products(connection):
     
     return response
 
+def insert_new_product(connection,product):
+    cursor=connection.cursor()
+    
+    query=("insert into products(name,uom_id,price_per_unit) values (%s,%s,%s)")
+    
+    data=(product['product_name'],product['uom_id'],product['price_per_unit'])
+    
+    cursor.execute(query,data)
+    connection.commit()
+
+    return cursor.lastrowid
+
+def delete_product(connection,product_id):
+    cursor=connection.cursor()
+    
+    query=("delete from products where product_id="+str(product_id ))
+    cursor.execute(query)
+    connection.commit()
+
+
 
 if __name__ =='__main__':
     connection=get_sql_connection()
-    print(get_all_products(connection))
+    print(delete_product(connection,9))
+    
